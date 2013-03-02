6c5f84809f13ba3e16b1226eaa5a034f1e813de9
Kristaps Kohs <kristaps.kohs@gmail.com>
2013-03-02 18:12:42 +0200

    project layout

--- anabode-game/desktop/src/com/anabode/desktop/AnabodeDesktopLauncher.java
+++ anabode-game/desktop/src/com/anabode/desktop/AnabodeDesktopLauncher.java
@@ -4,6 +4,8 @@ import com.anabode.controller.SceneController;
 import com.anabode.screen.ScreenHandler;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 
+import java.util.prefs.Preferences;
+
 /**
  * @author Kristaps Kohs
  */
@@ -11,6 +13,10 @@ public class AnabodeDesktopLauncher {
     public static void main(String[] args) {
         ScreenHandler screenHandler = new ScreenHandler();
         screenHandler.setController(new SceneController());
-        new LwjglApplication(screenHandler, "Anabode", 800, 600, true);
+
+        int width = Preferences.userRoot().getInt("anabode.screen.width", 800);
+        int height = Preferences.userRoot().getInt("anabode.screen.height", 600);
+
+        new LwjglApplication(screenHandler, "Anabode", width, height, true);
     }
 }
